9bfffb1e17df3aa619d6cf483e92183098e493ee
dowol <dowol.dev@gmail.com>
2026-01-30 23:37:38 +0900

    Modify vite config

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
     },
 
     devOptions: {
-      enabled: false,
+      enabled: true,
       navigateFallback: 'index.html',
       suppressWarnings: true,
       type: 'module',
