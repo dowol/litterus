ac97d31b9dca1576a18bd6f4759c9771810de354
dowol <dowol.dev@gmail.com>
2026-02-01 02:36:20 +0900

    Modify vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import {name} from './package.json' with {type: 'json'};
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: import.meta.env.PROD ? `/${name}/` : undefined,
     build: {
         sourcemap: false,
         rollupOptions: {
